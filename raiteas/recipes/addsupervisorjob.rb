caca723b6be99c7ac0cdda4dff741877131e12ca
Crashthatch <fletcher.tom@gmail.com>
2013-08-22 00:41:35 +0100

    hard code path

--- raiteas/recipes/addsupervisorjob.rb
+++ raiteas/recipes/addsupervisorjob.rb
@@ -1,6 +1,12 @@
 include_recipe 'supervisor'
   
 supervisor_service "pythonserver" do
+  command "python /srv/www/raiteas_pythonserver/current/app/app.py runserver live"
+  numprocs 1
+  stdout_logfile "~/python.out"
+  stderr_logfile "~/python.err"
+  autostart true
+  autorestart true
+  user "ubuntu"
   action :enable
-  command "python "+deploy[:deploy_to]+"/app/app.py runserver live"
 end
