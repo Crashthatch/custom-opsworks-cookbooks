e2d27176ed609f7f08e5f76af3e1635e16974aab
Crashthatch <fletcher.tom@gmail.com>
2013-08-21 23:02:52 +0100

    Use action :run so that pip actually gets executed. Add supervisor job.

--- raiteas/recipes/pythonserver.rb
+++ raiteas/recipes/pythonserver.rb
@@ -1,4 +1,5 @@
 include_recipe 'deploy'
+include_recipe 'supervisor'
 
 node[:deploy].each do |application, deploy|
   if deploy[:application] != 'raiteas_pythonserver'
@@ -25,6 +26,17 @@ node[:deploy].each do |application, deploy|
   execute "run pip install for #{application}" do
     cwd deploy[:deploy_to]
     command "pip install requirements.txt"
-    action :nothing
+    action :run
+  end
+  
+  supervisor_service "pythonserver" do
+    action :enable
+    command "python "+deploy[:deploy_to]+"/app/app.py runserver live"
+    numprocs 1
+    stdout_logfile "~/python.out"
+    stderr_logfile "~/python.err"
+    autostart true
+    autorestart true
+    user "ubuntu"
   end
 end
