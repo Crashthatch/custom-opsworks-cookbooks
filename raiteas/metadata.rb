2fb9572396917d7986dc477325caa2a1ec7315ab
Crashthatch <fletcher.tom@gmail.com>
2013-08-22 00:01:34 +0100

    Add supervisor job recipe.

--- raiteas/metadata.rb
+++ raiteas/metadata.rb
@@ -2,5 +2,6 @@ maintainer "Cambridge Data Solutions"
 description "Deploy applications"
 version "0.1"
 recipe "raiteas::pythonserver", "Deploy the Python SVG server"
+recipe "raiteas::addsupervisorjob", "Create a supervisor job"
 
 depends "deploy"
---
+++ raiteas/recipes/addsupervisorjob.rb
@@ -0,0 +1,12 @@
+include_recipe 'supervisor'  
+  
+supervisor_service "pythonserver" do
+  action :enable
+  command "python "+deploy[:deploy_to]+"/app/app.py runserver live"
+  numprocs 1
+  stdout_logfile "~/python.out"
+  stderr_logfile "~/python.err"
+  autostart true
+  autorestart true
+  user "ubuntu"
+end
