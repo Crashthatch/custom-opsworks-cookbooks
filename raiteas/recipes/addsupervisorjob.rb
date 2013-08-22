6d38617a022cb12cf189234e29c7101e686888d4
Crashthatch <fletcher.tom@gmail.com>
2013-08-22 12:57:32 +0100

    Enclose dir with quotes.

--- raiteas/recipes/addsupervisorjob.rb
+++ raiteas/recipes/addsupervisorjob.rb
@@ -1,7 +1,7 @@
 include_recipe 'supervisor'
   
 supervisor_service "pythonserver" do
-  directory /srv/www/raiteas_pythonserver/current/app
+  directory "/srv/www/raiteas_pythonserver/current/app"
   command "python app.py runserver live"
   numprocs 1
   stdout_logfile "/home/ubuntu/python.out"
