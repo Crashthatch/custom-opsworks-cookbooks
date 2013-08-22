60d2b3df5eada9251991b8429829390d8aa1e49b
Crashthatch <fletcher.tom@gmail.com>
2013-08-22 12:44:34 +0100

    Start in the correct directory.

--- raiteas/recipes/addsupervisorjob.rb
+++ raiteas/recipes/addsupervisorjob.rb
@@ -1,10 +1,11 @@
 include_recipe 'supervisor'
   
 supervisor_service "pythonserver" do
-  command "python /srv/www/raiteas_pythonserver/current/app/app.py runserver live"
+  directory /srv/www/raiteas_pythonserver/current/app
+  command "python app.py runserver live"
   numprocs 1
-  stdout_logfile "~/python.out"
-  stderr_logfile "~/python.err"
+  stdout_logfile "/home/ubuntu/python.out"
+  stderr_logfile "/home/ubuntu/python.err"
   autostart true
   autorestart true
   user "ubuntu"
